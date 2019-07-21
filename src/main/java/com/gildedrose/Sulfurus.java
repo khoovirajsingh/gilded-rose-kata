f04d51a4df08ace0d98d42c305492c0011499c79
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-07-21 14:47:13 +0100

    Override doUpdateQuality in subclasses

--- src/main/java/com/gildedrose/Sulfurus.java
+++ src/main/java/com/gildedrose/Sulfurus.java
@@ -4,4 +4,9 @@ public class Sulfurus extends Item {
     public Sulfurus(int sellIn, int quality) {
         super("Sulfuras, Hand of Ragnaros", sellIn, quality);
     }
+
+    @Override
+    void doUpdateQuality() {
+        super.doUpdateQuality();
+    }
 }
