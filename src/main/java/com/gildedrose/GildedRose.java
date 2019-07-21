c0b9187f418be2d1134546d31c5225d530699c55
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-07-21 13:24:40 +0100

    Used Lift-up conditional refactoring to group all logic for aged brie under one if statement

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,18 +15,30 @@ class GildedRose {
     }
 
     private void doUpdateQuality(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
+        if (item.name.equals("Aged Brie")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            }
+
+            item.sellIn = item.sellIn - 1;
+
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        } else {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
                     if (item.sellIn < 11) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
@@ -40,14 +52,12 @@ class GildedRose {
                     }
                 }
             }
-        }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
+            }
 
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
+            if (item.sellIn < 0) {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (item.quality > 0) {
                         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -57,11 +67,8 @@ class GildedRose {
                 } else {
                     item.quality = item.quality - item.quality;
                 }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
             }
         }
     }
+
 }
