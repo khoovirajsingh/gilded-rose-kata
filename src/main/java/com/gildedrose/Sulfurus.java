df2184a836a5f01db703bc7e660e05797b7c013e
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-07-21 13:33:06 +0100

    Added subclasses of items

---
+++ src/main/java/com/gildedrose/Sulfurus.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+public class Sulfurus extends Item {
+    public Sulfurus(int sellIn, int quality) {
+        super("Sulfuras, Hand of Ragnaros", sellIn, quality);
+    }
+}
