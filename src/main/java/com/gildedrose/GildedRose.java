ea175db53dca614e1c14c491cdb88baa6698f408
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-07-21 13:28:00 +0100

    Used Lift-up conditional refactoring on sulfurus

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -52,19 +52,17 @@ class GildedRose {
                     item.quality = item.quality - item.quality;
                 }
             } else {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+
+                } else {
+                    if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
-                }
 
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                     item.sellIn = item.sellIn - 1;
-                }
 
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
                             item.quality = item.quality - 1;
                         }
                     }
