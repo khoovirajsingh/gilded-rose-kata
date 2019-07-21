abca6bf240c0875d63193b7629bc51cc54fa0032
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-07-21 14:52:56 +0100

    Replaced for loop with for each

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,8 +8,8 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            items[i].doUpdateQuality();
+        for (Item item : items) {
+            item.doUpdateQuality();
         }
     }
 }
